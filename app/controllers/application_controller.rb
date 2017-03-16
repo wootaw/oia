4671f6477c315bdc8b3628b6856f87db8c7f3039
wootaw <wootaw@gmail.com>
2017-03-16 15:11:18 +0800

    Add 404 and 403 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,30 @@ class ApplicationController < ActionController::Base
       @owner = Team.find_by(name: params[:owner_name]) if @owner.nil?
     end
   end
+
+  def set_project
+    @project = if @owner.is_a?(User)
+                @owner.personal_projects.find_by(name: params[:project_name])
+              end
+  end
+
+  def render_404
+    render_optional_error_file(404)
+  end
+
+  def render_403
+    render_optional_error_file(403)
+  end
+
+  def render_optional_error_file(status_code)
+    status = status_code.to_s
+    fname = %w(404 403 422 500).include?(status) ? status : 'unknown'
+    
+    respond_to do |format|
+      format.html { render template: "/errors/#{fname}", status: status }
+      format.all  { render nothing: true, status: status }
+    end
+  end
   
   protected
 
