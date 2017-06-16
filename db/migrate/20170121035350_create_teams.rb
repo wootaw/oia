9384693833486c41028026b144aab9bff107b5ff
wootaw <wootaw@gmail.com>
2017-06-16 11:10:17 +0800

    fixed an error that the migration of teams repeat to create index by name

--- db/migrate/20170121035350_create_teams.rb
+++ db/migrate/20170121035350_create_teams.rb
@@ -1,7 +1,7 @@
 class CreateTeams < ActiveRecord::Migration[5.0]
   def change
     create_table :teams do |t|
-      t.string      :name, index: true
+      t.string      :name
       t.string      :summary
       t.integer     :state, index: true
       t.timestamps
