f6fefe7269c3156950fa8b09373c01e0288eca46
wootaw <wootaw@gmail.com>
2017-06-21 10:36:02 +0800

    Optimize page load speed

--- frontend/build/webpack.prod.conf.js
+++ frontend/build/webpack.prod.conf.js
@@ -1,5 +1,8 @@
 var config = require('./webpack.base.conf');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack = require('webpack');
+// var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+var CompressionPlugin = require("compression-webpack-plugin");
 
 config.devtool = 'cheap-module-eval-source-map';
 
@@ -18,9 +21,35 @@ config.module.loaders.push({
 });
 
 config.plugins.push(
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: '"production"'
+    }
+  }),
+  
   new ExtractTextPlugin({
     filename: '[name]_bundle-[chunkhash].css',
     allChunks: true
+  }),
+
+  new CompressionPlugin({
+    asset: "[path].gz[query]",
+    algorithm: "gzip",
+    test: /\.(js|html)$/,
+    threshold: 10240,
+    minRatio: 0.8
+  }),
+  
+  // new BundleAnalyzerPlugin(),
+
+  // new webpack.optimize.DedupePlugin(),
+
+
+  new webpack.optimize.UglifyJsPlugin({
+    sourceMap: true,
+    compress: {
+      warnings: false
+    }
   })
 );
 
